ecb359482f5b51b688030bc030edd4de30a60c90
user-AC <117973717+user-AC@users.noreply.github.com>
2024-08-13 18:27:06 +0800

    Update run_download_extract.sh

--- claude_arxiv_download_extarct/run_download_extract.sh
+++ claude_arxiv_download_extarct/run_download_extract.sh
@@ -6,5 +6,5 @@ pdf_path="./arxiv_pdfs_${st}_to_${ed}"
 
 python pdf-download-with-cache.py --start_date=${st} --end_date=${ed} --subject="cs"
 
-python pdf_to_table_figures.py --pdf_path=${pdf_path}
+python pdf_to_table_figures.py --pdf_path=${pdf_path} # --model_id="yifeihu/TF-ID-large" if network is ok
 
