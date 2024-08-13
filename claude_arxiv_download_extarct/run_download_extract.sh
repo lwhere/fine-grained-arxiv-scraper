dfbca7b70f6561a6bd6ffd2f0d1743cc852a1b2c
user-AC <117973717+user-AC@users.noreply.github.com>
2024-08-13 18:25:17 +0800

    Update run_download_extract.sh

--- claude_arxiv_download_extarct/run_download_extract.sh
+++ claude_arxiv_download_extarct/run_download_extract.sh
@@ -4,7 +4,7 @@ st="2024-08-04"
 ed="2024-08-05"
 pdf_path="./arxiv_pdfs_${st}_to_${ed}"
 
-# python pdf-download-with-cache.py --start_date=${st} --end_date=${ed} --subject="cs"
+python pdf-download-with-cache.py --start_date=${st} --end_date=${ed} --subject="cs"
 
 python pdf_to_table_figures.py --pdf_path=${pdf_path}
 
