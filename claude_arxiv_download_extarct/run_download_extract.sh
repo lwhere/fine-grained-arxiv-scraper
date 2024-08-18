3b614c23dbc1a719a3bd1ff8a9b082d248607c85
user-AC <117973717+user-AC@users.noreply.github.com>
2024-08-19 07:41:56 +0800

    Update run_download_extract.sh

--- claude_arxiv_download_extarct/run_download_extract.sh
+++ claude_arxiv_download_extarct/run_download_extract.sh
@@ -6,5 +6,13 @@ pdf_path="./arxiv_pdfs_${st}_to_${ed}"
 
 python pdf-download-with-cache.py --start_date=${st} --end_date=${ed} --subject="cs"
 
-python pdf_to_table_figures.py --pdf_path=${pdf_path} # --model_id="yifeihu/TF-ID-large" if network is ok
+# # Load the pre-trained model from Hugging Face
+# python pdf_to_table_figures.py --pdf_path=${pdf_path} --model_id="yifeihu/TF-ID-large"
+# python pdf_to_table_figures.py --pdf_path=${pdf_path} --model_id="yifeihu/TF-ID-large-no-caption"
 
+# Load the already downloaded pre-trained model from ./yifeihu/TF-ID-large or ./yifeihu/TF-ID-large-no-caption
+# with-caption
+python pdf_to_table_figures.py --pdf_path=${pdf_path} --model_id="./yifeihu/TF-ID-large"
+
+# without-caption
+python pdf_to_table_figures.py --pdf_path=${pdf_path} --model_id="./yifeihu/TF-ID-large-no-caption"
